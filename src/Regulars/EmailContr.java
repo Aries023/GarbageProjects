97509d109adc9b8841d1e3924577d4edda88becf
Aries023 <pet.potocka@gmail.com>
2022-06-26 02:52:28 +0200

    EmailContr class => add emailPasswordCont method.

--- src/Regulars/EmailContr.java
+++ src/Regulars/EmailContr.java
@@ -4,4 +4,8 @@ public class EmailContr {
     public boolean emailControlerSet (String arg){
         return arg.matches("\\w+@\\w+\\.(sk|cz|com|org)");
     }
+
+    public boolean emailPasswordCont(String arg){
+        return arg.matches(".*\\d.*") && arg.matches(".*[A-Z].*") && arg.length()>=7;
+    }
 }
