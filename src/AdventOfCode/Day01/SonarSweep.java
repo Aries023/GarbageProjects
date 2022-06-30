bc209aad41d333ba479f632e6fa494ab2b7af55a
Aries023 <pet.potocka@gmail.com>
2022-07-01 00:04:36 +0200

    I made method howManyIncrements inside SonarSweep;

--- src/AdventOfCode/Day01/SonarSweep.java
+++ src/AdventOfCode/Day01/SonarSweep.java
@@ -1,4 +1,22 @@
 package AdventOfCode.Day01;
 
 public class SonarSweep {
+    public void howManyIncrements(String arg){
+        int increments = 0;
+        boolean first = true;
+        int a=0;
+        int b;
+        String [] arrayRay = arg.split("\\n");
+        for (String s :arrayRay){
+       b = Integer.parseInt(s);
+       if (first){
+           a = b;
+           first=false;
+       }
+       if (a<b)
+           ++increments;
+       a = b;
+        }
+        System.out.println(increments);
+    }
 }
