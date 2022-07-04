54b2cea7719155ba8fcc3fa24f95611bc1f177e2
Aries023 <pet.potocka@gmail.com>
2022-07-04 11:11:30 +0200

    add some coments for better read code

--- src/AdventOfCode/Day01/SonarSweep.java
+++ src/AdventOfCode/Day01/SonarSweep.java
@@ -2,20 +2,29 @@ package AdventOfCode.Day01;
 
 public class SonarSweep {
     public void howManyIncrements(String arg){
-        int increments = 0;
-        boolean first = true;
-        int a=0;
-        int b;
+
+        int increments = 0;  // how many increments
+        boolean first = true; // if i found first number ( int b is not inicialized yet)
+
+        int a=0; // last number
+        int b; // actual number
+
+        // spilit String lines and put it into array (pole)
         String [] arrayRay = arg.split("\\n");
+
         for (String s :arrayRay){
-       b = Integer.parseInt(s);
+       b = Integer.parseInt(s); // parase String to int, there could be a ERROR!! its not into try/catch
+
+            // if you get first number
        if (first){
            a = b;
            first=false;
        }
+       // if actual number is bigest then last, increment ++
        if (a<b)
            ++increments;
-       a = b;
+
+       a = b; // actual number become to last number
         }
         System.out.println(increments);
     }
