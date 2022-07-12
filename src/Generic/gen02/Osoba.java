c544152ee175bc19403b1dafb008662099ee9b2d
Aries023 <pet.potocka@gmail.com>
2022-07-13 00:00:18 +0200

    maded class Osoba

---
+++ src/Generic/gen02/Osoba.java
@@ -0,0 +1,15 @@
+package Generic.gen02;
+
+public class Osoba {
+    String firstName;
+    String lastName;
+    byte age;
+
+    public Osoba(){}
+
+    public Osoba(String firstName, String lastName, byte age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+}
