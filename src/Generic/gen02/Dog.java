3cf250aa1ef2f87c1d4e2da577da7590b9fe9266
Aries023 <pet.potocka@gmail.com>
2022-07-13 00:28:08 +0200

    maded class Dog

---
+++ src/Generic/gen02/Dog.java
@@ -0,0 +1,13 @@
+package Generic.gen02;
+
+public class Dog {
+    String name;
+
+    public void setName(String arg){
+        name = arg;
+    }
+
+    public void bark(){
+        System.out.println("Bark bark, BARK!");
+    }
+}
