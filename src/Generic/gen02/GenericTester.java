4bf39eb964361de59b8e1dc23a758b1c34ca53cf
Aries023 <pet.potocka@gmail.com>
2022-07-13 00:30:58 +0200

    maded GenericTester class and make generic <P extends Dog>

---
+++ src/Generic/gen02/GenericTester.java
@@ -0,0 +1,13 @@
+package Generic.gen02;
+
+public class GenericTester <O extends Dog>{
+    private O object;
+
+    public O getObject() {
+        return object;
+    }
+
+    public void setObject(O object) {
+        this.object = object;
+    }
+}
