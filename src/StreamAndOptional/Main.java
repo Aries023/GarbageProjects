51bb7ff8e844e4a042daf79d405addfd08dc4082
Aries023 <pet.potocka@gmail.com>
2022-07-31 21:32:46 +0200

    repaired last comit and add one list of Osoba

---
+++ src/StreamAndOptional/Main.java
@@ -0,0 +1,10 @@
+package StreamAndOptional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List <Osoba> osoby = new ArrayList<>();
+    }
+}
---
+++ src/StreamAndOptional/Osoba.java
@@ -0,0 +1,4 @@
+package StreamAndOptional;
+
+public class Osoba {
+}
