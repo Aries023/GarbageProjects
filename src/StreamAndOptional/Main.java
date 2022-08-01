685171a610bb2494d885ab3513b47666fd8b8d37
Aries023 <pet.potocka@gmail.com>
2022-08-01 20:33:31 +0200

    vlozenie listu do streamu a mensie prefiltrovanie + dorobenie Class Osoba

--- src/StreamAndOptional/Main.java
+++ src/StreamAndOptional/Main.java
@@ -1,10 +1,31 @@
 package StreamAndOptional;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List <Osoba> osoby = new ArrayList<>();
+
+        Osoba o = new Osoba("kaly","Retreat", 20); // na skusku .distract (jeden objek zobrazi iba raz)
+
+        List <Osoba> osoby = Arrays.asList(
+                new Osoba("Peter", "Poty",29),
+                new Osoba("Mato", "Facho",34),
+                new Osoba("Kika", "Mlada",18), // 1 Kika..
+                new Osoba("Kika", "Mlada",18), // 2 ina Kika.. !
+                o, // 1
+                o, // 2
+                new Osoba("Marek", "keks",60)
+        );
+
+
+
+        osoby.stream()  // vytvorenie streamu (pruud)
+                .filter(os -> os.getAge()<30) // prefiltrovanie (iba pod 30 rokov
+                .distinct() // kazdy objekt (osoba) moze byt iba raz
+                .sorted(Comparator.comparing(Osoba::getAge)) // zoradenie podla veku
+                .forEach(k -> System.out.println(k.getName() + " " +k.getLastName() + " " + k.getAge() + " Kompletna osoba: " + k));
     }
 }
--- src/StreamAndOptional/Osoba.java
+++ src/StreamAndOptional/Osoba.java
@@ -1,4 +1,27 @@
 package StreamAndOptional;
 
 public class Osoba {
+    String name;
+    String lastName;
+    int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Osoba(String name, String lastName, int age) {
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
+
+
+    }
 }
